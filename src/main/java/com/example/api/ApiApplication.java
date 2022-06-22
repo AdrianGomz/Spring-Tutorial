567d4a6d299226ae74ca70efed60830ea1131ae6
isaac <vi13150@innovaccion.mx>
2022-06-21 22:01:59 -0600

    Added Student Controller

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -1,27 +1,13 @@
 package com.example.api;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.api.student.Student;
 
 @SpringBootApplication
-@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(new Student(1L, "Mariam", "mariam@gmail", LocalDate.of(200, Month.JANUARY, 5), 21));
 	}
 
 }
